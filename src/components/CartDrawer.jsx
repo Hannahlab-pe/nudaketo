29b28dafcaa30707c93d53baaaad54bfc174692a
Hannahlab-pe <operaciones@hannahlab.com>
2026-06-16 10:33:12 -0500

    fix: boton de pago dice 'Pagar S/monto' (Culqi ofrece mas que tarjeta)

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -344,7 +344,7 @@ export default function CartDrawer() {
                       ) : (
                         <>
                           <IconCard />
-                          Pagar con tarjeta
+                          Pagar S/{total.toFixed(2)}
                         </>
                       )}
                     </button>
